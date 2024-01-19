b6d00793a64db497774d4d53442d3ddd6ec27360
Elmo <37273327+Elmo9807@users.noreply.github.com>
2024-01-19 20:28:13 +0000

    Add files via upload

--- C2F.py
+++ C2F.py
@@ -1,4 +1,5 @@
-#Convert Elevator Floors
+#Convert Celisus to Fahrenheit, personal challenge.
+
 Celsius = int(input('Degrees Celsius:\n'))
 Fahrenheit = (1.8 * Celsius) + 32
 print('Temperature in Fahrenheit :' , Fahrenheit)
