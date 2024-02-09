46709fd70320ca69075ae09a762c7e0316303b80
Elmo <37273327+Elmo9807@users.noreply.github.com>
2024-02-09 03:01:03 +0000

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,3 +1,4 @@
+# Intended to read and print from a specific file, replace directory location with desired file input if you need to test the function of the code.
 f = open(r"C:\Users\dylan\Documents\hontzu.txt", "r")
 text = f.read()
 print(text)
