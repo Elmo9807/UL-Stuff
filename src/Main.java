6fa39c043e634a758e2c10b45bccc553104bc876
Elmo <37273327+Elmo9807@users.noreply.github.com>
2024-09-24 00:41:22 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,16 @@ public class Main {
         //Whatever();
         //question6();
         //question7();
-        question8();
+        //question8();
+        //question9();
+        //question10();
+        //question11();
+
+//        Question1 q1 = new Question1();
+//        q1.divMethod1();
+
+//        Question2 q2 = new Question2();
+//        q2.modMethod1();
     }
     static void divMethod() {
         System.out.println("Answer 1 is " + (9/4));
@@ -43,12 +52,57 @@ public class Main {
         System.out.println(leftDgt + ":" + rightDgt);
     }
     static void question8() {
-        int preVatPrice = 5500; //Used int to store whole number
-        double VAT_RATE = 0.21; //Used double to store decimal value
-        double VAT = preVatPrice * VAT_RATE; //Used double for decimal value, calculating VAT amount due
-        double withVat = preVatPrice + VAT;
+        //Variable declaration and initialisation
+        int preVatPrice = 5500;
+        double vatRate = preVatPrice * 0.21; // VAT calculation
 
-        System.out.println("Pre VAT Price: 5500");
-        System.out.printf();
+        //Price with VAT calculated
+        double priceWithVat = preVatPrice + vatRate;
+
+        System.out.println("Price before VAT: " + preVatPrice); //Print out Price Before VAT
+        System.out.println("VAT at 21% is: " + vatRate); //VAT % calculated from given preVatPrice
+        System.out.println("Price with VAT: " + priceWithVat); //Price with VAT printed from calculation in variable
+
+    }
+    static void question9() {
+        int heightInInches = 73; //Declare and initialise variable heightInInches as int value
+
+        int feet = heightInInches / 12; //Acquire height in feet by dividing inches height by 12
+        int inches = heightInInches % 12; //Use mod to get the remaining inches
+
+        System.out.println("Your height in feet and inches is: " + feet + "'" + inches + "\"");
+        //Print statement with concatenated str and int values denoting height in feet and inches. Hopefully it works.
+    }
+    static void question10() {
+        int partyGuests = 65; //declare variable and initialise for partyGuests
+        int bottlesPerGuest = 1; //declare and initialise variable for bottlesPerGuest
+        int bottlesPerBox = 25; //declare and initialise variable for bottlesPerBox
+        System.out.println("You will require " + (partyGuests * bottlesPerGuest / bottlesPerBox) + " whole boxes and "
+                + (partyGuests * bottlesPerGuest % bottlesPerBox) + " extra bottles.");
+        //Print statement with str concatenated with expressions for whole boxes and extra bottles required.
+        //Sub in 2 for bottlesPerGuest to answer question 10.1
+    }
+    static void question11() {
+        int withdrawalAmount = 80;
+        int fifties, twenties, tens, fives;
+
+        fifties = withdrawalAmount / 50;
+        withdrawalAmount = withdrawalAmount % 50;
+
+        twenties = withdrawalAmount / 20;
+        withdrawalAmount = withdrawalAmount % 20;
+
+        tens = withdrawalAmount / 10;
+        withdrawalAmount = withdrawalAmount % 10;
+
+        fives = withdrawalAmount / 5;
+        withdrawalAmount = withdrawalAmount % 5;
+
+        System.out.println("You will receive: ");
+        System.out.println(fifties + " €50 note(s)");
+        System.out.println(twenties + " €20 note(s)");
+        System.out.println(tens + " €10 note(s)");
+        System.out.println(fives + " €5 note(s)");
+
     }
 }
